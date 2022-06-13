c4c67fa7c59e535e7193c83af5051c34c7a96054
Ryosuke Okada <ryosuke.okada@fujitsu.com>
2022-06-13 16:19:04 +0900

    cli: output directory incorrect when input directory ends in separator
    Fixes #317
    
    Signed-off-by: Ryosuke Okada <ryosuke.okada@fujitsu.com>
    Signed-off-by: BJ Hargrave <hargrave@us.ibm.com>

--- org.eclipse.transformer.cli/src/test/java/transformer/test/TestCommandLine.java
+++ org.eclipse.transformer.cli/src/test/java/transformer/test/TestCommandLine.java
@@ -12,18 +12,18 @@
 package transformer.test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.PrintStream;
 import java.util.Properties;
 
+import aQute.lib.io.IO;
 import org.assertj.core.api.SoftAssertions;
 import org.eclipse.transformer.Transformer;
 import org.eclipse.transformer.action.Changes;
 import org.eclipse.transformer.action.ContainerChanges;
+import org.eclipse.transformer.action.impl.DirectoryActionImpl;
 import org.eclipse.transformer.action.impl.JavaActionImpl;
 import org.eclipse.transformer.action.impl.ManifestActionImpl;
 import org.eclipse.transformer.action.impl.ZipActionImpl;
@@ -40,7 +40,7 @@ import org.slf4j.MarkerFactory;
 class TestCommandLine {
 
 	private static final String	STATIC_CONTENT_DIR			= "src/test/data/command-line";
-	private static final String DYNAMIC_CONTENT_DIR          = "target/test/data/command-line";
+	private String DYNAMIC_CONTENT_DIR;
 
 	private String currentDirectory	= ".";
 	private Properties			prior;
@@ -54,6 +54,7 @@ class TestCommandLine {
 			+ testInfo.getTestMethod()
 				.get()
 				.getName();
+		DYNAMIC_CONTENT_DIR = "target/test/" + name;
 		prior = new Properties();
 		prior.putAll(System.getProperties());
 
@@ -75,14 +76,49 @@ class TestCommandLine {
 	void testManifestActionAccepted() throws Exception {
 		String inputFileName = STATIC_CONTENT_DIR + '/' + "MANIFEST.MF";
 		String outputFileName = DYNAMIC_CONTENT_DIR + '/' + "MANIFEST.MF";
-		verifyAction(ManifestActionImpl.class.getName(), inputFileName, outputFileName);
+		verifyAction(ManifestActionImpl.class.getName(), inputFileName, outputFileName, outputFileName);
 	}
 
 	@Test
 	void testJavaActionAccepted() throws Exception {
 		String inputFileName = STATIC_CONTENT_DIR + '/' + "A.java";
 		String outputFileName = DYNAMIC_CONTENT_DIR + '/' + "A.java";
-		verifyAction(JavaActionImpl.class.getName(), inputFileName, outputFileName);
+		verifyAction(JavaActionImpl.class.getName(), inputFileName, outputFileName, outputFileName);
+	}
+
+	@Test
+	void testInputDirectoryNameOnlyAccepted() throws Exception {
+		File inputFile = IO.copy(new File(STATIC_CONTENT_DIR), new File(DYNAMIC_CONTENT_DIR));
+		String inputFileName = inputFile.getCanonicalPath().replace(File.separatorChar, '/');
+		String expectedOutputFileName = new File(inputFile.getParentFile(), Transformer.OUTPUT_PREFIX + inputFile.getName()).getCanonicalPath()
+			.replace(File.separatorChar, '/');
+		verifyAction(DirectoryActionImpl.class.getName(), inputFileName, null, expectedOutputFileName, 3);
+	}
+
+	@Test
+	void testInputDirectoryNameOnlyWithLastSlashAccepted() throws Exception {
+		File inputFile = IO.copy(new File(STATIC_CONTENT_DIR), new File(DYNAMIC_CONTENT_DIR));
+		String inputFileName = inputFile.getCanonicalPath().replace(File.separatorChar, '/') + "/";
+		String expectedOutputFileName = new File(inputFile.getParentFile(), Transformer.OUTPUT_PREFIX + inputFile.getName()).getCanonicalPath()
+			.replace(File.separatorChar, '/');
+		verifyAction(DirectoryActionImpl.class.getName(), inputFileName, null, expectedOutputFileName, 3);
+	}
+
+	@Test
+	void testInputFileNameOnlyAccepted() throws Exception {
+		File inputFile = new File(IO.copy(new File(STATIC_CONTENT_DIR), new File(DYNAMIC_CONTENT_DIR)), "A.java");
+		String inputFileName = inputFile.getCanonicalPath().replace(File.separatorChar, '/');
+		String expectedOutputFileName = new File(inputFile.getParentFile(), Transformer.OUTPUT_PREFIX + inputFile.getName()).getCanonicalPath()
+			.replace(File.separatorChar, '/');
+		verifyAction(JavaActionImpl.class.getName(), inputFileName, null, expectedOutputFileName);
+	}
+
+	@Test
+	void testInvalidOutputDirectoryRejected() throws Exception {
+		IO.copy(new File(STATIC_CONTENT_DIR), new File(DYNAMIC_CONTENT_DIR));
+		String inputFileName = DYNAMIC_CONTENT_DIR + '/';
+		String outputFileName = DYNAMIC_CONTENT_DIR + '/' + "foo";
+		verifyActionInvalidDirectoryRejected(DirectoryActionImpl.class.getName(), inputFileName, outputFileName);
 	}
 
 	// Test compressed zip with copying to make sure ZipEntries are properly created.
@@ -90,7 +126,7 @@ class TestCommandLine {
 	void zip_entry_creation() throws Exception {
 		String inputFileName = STATIC_CONTENT_DIR + '/' + "sac-1.3.jar";
 		String outputFileName = DYNAMIC_CONTENT_DIR + '/' + "sac-1.3.jar";
-		verifyAction(ZipActionImpl.class.getName(), inputFileName, outputFileName);
+		verifyAction(ZipActionImpl.class.getName(), inputFileName, outputFileName, outputFileName);
 	}
 
 	// Test war with duplicate entries.
@@ -98,7 +134,7 @@ class TestCommandLine {
 	void duplicate_entries() throws Exception {
 		String inputFileName = STATIC_CONTENT_DIR + '/' + "servlet_plu_singlethreadmodel_web.war";
 		String outputFileName = DYNAMIC_CONTENT_DIR + '/' + "servlet_plu_singlethreadmodel_web.war";
-		verifyAction(ZipActionImpl.class.getName(), inputFileName, outputFileName, 3);
+		verifyAction(ZipActionImpl.class.getName(), inputFileName, outputFileName, outputFileName, 3);
 	}
 
 	@Test
@@ -200,41 +236,51 @@ class TestCommandLine {
 			// .contains("Test log other marker");
 			softly.assertThat(sysOut.toString())
 				.contains("Test log console marker")
-			.doesNotContain("Test error console marker")
-			.doesNotContain("Test log plain")
+				.doesNotContain("Test error console marker")
+				.doesNotContain("Test log plain")
 				.doesNotContain("Test log other marker");
 			softly.assertThat(sysErr.toString())
 				.doesNotContain("Test log console marker")
-			.contains("Test error console marker")
-			.doesNotContain("Test log plain")
+				.contains("Test error console marker")
+				.doesNotContain("Test log plain")
 				.doesNotContain("Test log other marker");
 		});
 
 	}
 
-	private void verifyAction(String actionClassName, String inputFileName, String outputFileName) throws Exception {
-		verifyAction(actionClassName, inputFileName, outputFileName, 0);
+	private void verifyAction(String actionClassName, String inputFileName, String outputFileName, String expectedOutputFileName) throws Exception {
+		verifyAction(actionClassName, inputFileName, outputFileName, expectedOutputFileName, 0);
 	}
 
-	private void verifyAction(String actionClassName, String inputFileName, String outputFileName, int duplicates) throws Exception {
+	private void verifyAction(String actionClassName, String inputFileName, String outputFileName, String expectedOutputFileName, int duplicates) throws Exception {
 		System.out.printf("verifyAction: Input is: [%s] Output is: [%s]\n", inputFileName, outputFileName);
-		TransformerCLI cli = new JakartaTransformerCLI(System.out, System.err, new String[] {
+		String[] args = outputFileName != null ? new String[] {
 			inputFileName, outputFileName, "-o"
-		});
+		} : new String[] {
+			inputFileName, "-o"
+		};
+
+		TransformerCLI cli = new JakartaTransformerCLI(System.out, System.err, args);
 
 		Transformer transformer = new Transformer(cli.getLogger(), cli);
 
-		assertTrue(transformer.setInput(), "options.setInput() failed");
-		assertEquals(inputFileName, transformer.getInputFileName(),
-			"input file name is not correct [" + transformer.getInputFileName() + "]");
+		assertThat(transformer.setInput()).as("options.setInput()")
+			.isTrue();
+		assertThat(transformer.getInputFileName()).as("input file name")
+			.isEqualTo(inputFileName);
 
-		assertTrue(transformer.setOutput(), "options.setOutput() failed");
-		assertEquals(outputFileName, transformer.getOutputFileName(),
-			"output file name is not correct [" + transformer.getOutputFileName() + "]");
+		assertThat(transformer.setOutput()).as("options.setOutput()")
+			.isTrue();
+		assertThat(transformer.getOutputFileName()).as("output file name")
+			.isEqualTo(expectedOutputFileName);
 
-		assertTrue(transformer.setRules(transformer.getImmediateData()), "options.setRules() failed");
-		assertTrue(transformer.acceptAction(), "options.acceptAction() failed");
-		assertEquals(actionClassName, transformer.acceptedAction.getClass().getName());
+		assertThat(transformer.setRules(transformer.getImmediateData())).as("options.setRules()")
+			.isTrue();
+		assertThat(transformer.acceptAction()).as("options.acceptAction()")
+			.isTrue();
+		assertThat(transformer.acceptedAction.getClass()
+			.getName()).as("action class name")
+			.isEqualTo(actionClassName);
 
 		transformer.transform();
 
@@ -244,10 +290,36 @@ class TestCommandLine {
 			int numDuplicated = containerChanges.getAllDuplicated();
 			int numFailed = containerChanges.getAllFailed();
 
-			assertThat(numDuplicated).as("Duplicates were processed").isEqualTo(duplicates);
-			assertThat(numFailed).as("Failures were processed").isZero();
+			assertThat(numDuplicated).as("Duplicates were processed")
+				.isEqualTo(duplicates);
+			assertThat(numFailed).as("Failures were processed")
+				.isZero();
 		}
 
-		assertTrue((new File(outputFileName)).exists(), "output file not created");
+		File outputFile = new File(expectedOutputFileName);
+		assertThat(outputFile).as("output file")
+			.exists();
+		if (outputFile.isDirectory()) {
+			assertThat(outputFile).as("output directory")
+				.isNotEmptyDirectory();
+		}
+	}
+
+	private void verifyActionInvalidDirectoryRejected(String actionClassName, String inputFileName, String outputFileName) throws Exception {
+
+		String[] args = new String[] {
+			inputFileName, outputFileName, "-o"
+		};
+		TransformerCLI cli = new JakartaTransformerCLI(System.out, System.err, args);
+
+		Transformer transformer = new Transformer(cli.getLogger(), cli);
+
+		assertThat(transformer.setInput()).as("options.setInput()")
+			.isTrue();
+		assertThat(transformer.getInputFileName()).as("input file name")
+			.isEqualTo(inputFileName);
+
+		assertThat(transformer.setOutput()).as("options.setOutput() unexpectedly succeeded")
+			.isFalse();
 	}
 }
